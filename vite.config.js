961325c84adeb091b015864f820a36a52d1440fe
YousofFarrag <yousof.farrag@gmail.com>
2025-06-18 13:06:00 +0200

    fix: remove base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/letrasarabe/', // Set base for GitHub Pages deployment
   plugins: [react()],
 })
