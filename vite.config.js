f6f3cfc1cdf823cf27bccf5dd93bd74a341d5550
YousofFarrag <yousof.farrag@gmail.com>
2025-06-20 09:44:26 +0200

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-   // <-- This must match your repo name!
   plugins: [react()],
-  //base: '/arabepresente2025/',*/
+  // base: '/arabepresente2025/', // REMOVE or comment out for Vercel
 });
